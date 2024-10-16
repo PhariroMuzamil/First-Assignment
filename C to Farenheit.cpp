0f7a2be36562ccb4c7465e8ecbfaf3b2338b3104
Muzamil Noor <muzamilphariro@gmail.com>
2024-10-16 10:22:32 +0500

    Add files via upload
    This program is used to convert Celsius to Fahrenheit temperature it is also an assignment task

---
+++ C to Farenheit.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+ 	float  celcius, fahrenheit;
+ 	printf("Enter temprature in Celcus:");
+ 	scanf("%f", &celcius);
+ 	
+ 	fahrenheit = ( celcius*9/5) + 32;
+ 	printf("Temp: in Fahrenheit is: %f",fahrenheit);
+getch();
+ 	return 0;
+ 	
+ 	
+ }
