ee63a2c61f7a23c1b04e1e8b08a832bbc0d1ebf7
Muzamil Noor <muzamilphariro@gmail.com>
2024-10-16 10:17:46 +0500

    Add files via upload
    This is C addition program which was given by Sir Owais for our assignment work

---
+++ Addition Program.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+   int main()
+   {
+   	int a,b;
+   	printf("Enter any Value for a:");
+   	scanf("%d",&a);
+   	printf("Enter other value for b:");
+   	scanf("%d",&b);
+   	int c=a+b;
+   	printf(" Total Sum is: %d", a+b);
+   	getch();
+   	
+      }
