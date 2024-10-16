ab4dcc4e958423069cf741652473b2e97e32c505
Muzamil Noor <muzamilphariro@gmail.com>
2024-10-16 10:20:13 +0500

    Add files via upload
    Its C square program. It can be used to calculate square of any number, it was made during assignment task

---
+++ Square.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+  int main()
+  {
+  	int z;
+  	 printf("Enter any Value to find its Square:");
+  	 scanf("%d",&z);
+  	 printf("Square of a number is %d", z*z);
+  	 getch();
+  	
+  }
